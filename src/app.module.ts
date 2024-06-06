e222dc1b070e895a917f5ad38ccaf55428ec6e9d
Artandrey <artandrey7777@gmail.com>
2024-06-06 03:03:40 +0300

    feat: imported modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SupabaseModule } from './modules/supabase/supabase.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ExceptionsModule } from './modules/exceptions/exceptions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SupabaseModule, AuthModule, ExceptionsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
